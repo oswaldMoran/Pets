9d7ffad34fa2aadf222975c2e8760678a648f4c7
oscarmoran <oscarmoran@effit.com.mx>
2022-06-03 00:39:13 -0500

    first code

---
+++ PetsClass.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetFamily
+{
+    internal class PetsClass
+    {
+        private readonly string ConexionWithSPets = ConfigurationManager.AppSettings["cnnStrValue"];
+        /// <summary>
+        /// test
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetNamePets()
+        {
+            List<string> mascotas = new List<string>();
+            try
+            {
+
+                using (SqlConnection conexion = new SqlConnection(ConexionWithSPets))
+                {
+                    conexion.Open();
+                    if (conexion.State == ConnectionState.Open)
+                    {
+                        SqlCommand comandoSql = new SqlCommand("SP_PetsMoran", conexion);
+                        comandoSql.CommandType = CommandType.StoredProcedure;
+                        SqlDataReader resultInDataReader = comandoSql.ExecuteReader();
+                        while (resultInDataReader.Read())
+                        {
+                            string name = resultInDataReader[0].ToString();
+                            mascotas.Add(name);
+                        }
+                    }
+
+                }
+                //
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("La conexion no sepudo realizar");
+            }
+
+            return mascotas;
+        }
+        /// <summary>
+        /// this method return false when the main transaction failed otherwise returns true.
+        /// </summary>
+        /// <returns>boolean</returns>
+        public bool InsertPets(string name,string description,String gender,bool isStillAlive)
+        {
+            bool res = false;
+            try
+            {
+                using (SqlConnection conexion = new SqlConnection(ConexionWithSPets))
+                {
+                    conexion.Open();
+                    if (conexion.State == ConnectionState.Open)
+                    {
+                        SqlCommand comandoSql = new SqlCommand("SP_InsertPet", conexion);
+                        comandoSql.Parameters.Add("@Name", SqlDbType.VarChar).Value = name;
+                        comandoSql.Parameters.Add("@Description ", SqlDbType.VarChar).Value = description;
+                        comandoSql.Parameters.Add("@Gender ", SqlDbType.VarChar).Value = gender;
+                        comandoSql.Parameters.Add("@IsStilAlive ", SqlDbType.Bit).Value = isStillAlive;
+
+                        comandoSql.CommandType = CommandType.StoredProcedure;
+                        int insertResult = comandoSql.ExecuteNonQuery();
+                        if (insertResult > 0)
+                        {
+                            res = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("La conexion no sepudo realizar");
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿
+//permirtir al usuario realizar una busqueda dentro de una lista string,en caso de no haber coincidencias
+//permitir al usuario ingresar un nuevo elemento, en caso de encontrar coincidencias,
+//mostrar el resultado al usuario
+
+using PetFamily;
+
+PetsClass petDbObj = new PetsClass();
+
+////bool comp = petDbObj.InsertPets("Los perritos", "perrillos que acaban de nacer","M",true);
+////Console.WriteLine(comp ? "se pudo":"no se pudo");
+bool resp = false;
+List<string> NameList = petDbObj.GetNamePets();
+List<string> coincidencias = new List<string>();    
+while (!resp)
+{
+    Console.WriteLine("ingresa tu busqueda:");
+    string nameDog = Console.ReadLine();
+
+
+    foreach (var name in NameList)
+    {
+        if (name.Contains(nameDog)) //determina si existe una coincidencia entre strings y regresa un boolean
+        {
+            coincidencias.Add(name);
+            resp = true;
+        }
+    }
+
+    if (!resp)
+    {
+        Console.WriteLine("No existen coicidencias");
+        Console.WriteLine("Ahora Ingresa el nombre:");
+        string newDog = Console.ReadLine();
+        NameList.Add(newDog);
+        Console.WriteLine($"el nombre {newDog} ha sido agregado");
+    }
+    else
+    {
+        foreach (var perros in coincidencias)
+        {
+            Console.WriteLine(perros);
+        }
+    }
+
+
+
+}
